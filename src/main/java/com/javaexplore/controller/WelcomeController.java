8bf922c4f251e50cf721ff3cde44a0cbdd38e634
mahan <prabhat.nist4@gmail.com>
2024-08-24 23:15:16 +0530

    first commit

---
+++ src/main/java/com/javaexplore/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.javaexplore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Java World";
+    }
+}
